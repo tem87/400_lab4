b155fd0e4d7b1f78619a1a09bd1a5fd8c9cbfc67
Farhan-Abbas <fabbas.ambition@gmail.com>
2025-02-10 16:45:40 -0700

    added ping website feature

--- src/ping_websites.sh
+++ src/ping_websites.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# List of popular websites to ping
+# list of popular websites to ping
 websites=("google.com" "facebook.com" "youtube.com" "amazon.com" "wikipedia.org")
 
-# Ping each website and display the results
+# ping each website and display the results
 for website in "${websites[@]}"; do
     echo "Pinging $website..."
-    ping -c 4 $website
+    ping -c 4 $website   # ping 4 times
     echo ""
 done
